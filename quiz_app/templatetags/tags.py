ee7debcafbd12420c9e3c3e93b728dab9536d2ef
Subhon <subhon@debian-BULLSEYE-live-builder-AMD64>
2022-09-18 00:21:14 +0500

    requirements added and answered question indicator

--- quiz_app/templatetags/tags.py
+++ quiz_app/templatetags/tags.py
@@ -49,3 +49,11 @@ def active(context, question_id: int, questions) -> str:
 	if question_id_from_url == checked_question_id:
 		return 'active'
 	return 'disabled'
+
+
+@register.simple_tag(takes_context=True)
+def answered_question(context, question):
+	user_answer = models.UserAnswer.objects.filter(user=context['request'].user, question=question)
+	if user_answer:
+		return mark_safe('<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16"><path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/></svg>')
+	return ''
